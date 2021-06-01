cec142611afc2c488fafd1e91556949754d560b1
Matt <58913953+ms5984@users.noreply.github.com>
2021-06-01 02:35:17 -0400

    start placeholders

---
+++ src/main/java/com/github/sanctum/clansradar/placeholders/MapExpansion.java
@@ -0,0 +1,22 @@
+package com.github.sanctum.clansradar.placeholders;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.jetbrains.annotations.NotNull;
+
+public final class MapExpansion extends PlaceholderExpansion {
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return null;
+    }
+}
